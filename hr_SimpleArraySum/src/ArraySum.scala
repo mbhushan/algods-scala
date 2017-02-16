fe73b5fb0ec7741d70572a4a54f8d368d80ad403
manibhushan <manibhushan.cs@gmail.com>
2017-02-15 21:33:16 -0800

    array sum scala

--- hr_SimpleArraySum/src/ArraySum.scala
+++ hr_SimpleArraySum/src/ArraySum.scala
@@ -5,4 +5,20 @@
 
 object ArraySum {
   
+  def main(args: Array[String]) {
+    var x = readLine()
+//    var n = io.Source.stdin.getLines().toString
+//    var text = io.Source.stdin.getLines().toList
+    var line = readLine()
+    
+    var total = 0
+    var arr = line.split(" ").map(_.toInt)
+    
+    for (i <- arr) {
+      //println(i)
+      total += i
+    }
+    
+    println("array sum: " + total)
+  }
 }
