4f3658978371ff4e6e531b490a0fd17144542346
manibhushan <manibhushan.cs@gmail.com>
2017-02-17 21:24:32 -0800

    solution checking - triplets

--- hr_CompareTriplets/src/Triplets.scala
+++ hr_CompareTriplets/src/Triplets.scala
@@ -5,4 +5,24 @@
 
 object Triplets {
   
+   def main(args: Array[String]) {
+        val sc = new java.util.Scanner (System.in);
+        var a0 = sc.nextInt();
+        var a1 = sc.nextInt();
+        var a2 = sc.nextInt();
+        var b0 = sc.nextInt();
+        var b1 = sc.nextInt();
+        var b2 = sc.nextInt();
+        
+        if (a0 != b0) {
+            print("1 ")
+        }
+         if (a1 != b1) {
+            print("1 ")
+        }
+         if (a2 != b2) {
+            print("1")
+        }
+        
+    }
 }
